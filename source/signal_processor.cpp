d631d4341e1ca1851758b5ef0f1656cb8e07f7ed
3hoot <vietessss@gmail.com>
2024-05-01 12:24:36 +0200

    fixed pybind issues, added matplotplusplus as a submodule

--- source/signal_processor.cpp
+++ source/signal_processor.cpp
@@ -1,12 +1,29 @@
 #include <pybind11/pybind11.h>
+#include <matplot/matplot.h>
+
+#include <cmath>
 
 namespace py = pybind11;
 
-int sum(int a, int b) { return a + b; }
+void testPlot()
+{
+    using namespace matplot;
+    std::vector<double> x = linspace(0, 2 * pi, 50);
 
-PYBIND11_MODULE(example, m)
+    std::vector<std::vector<double>> Y(2);
+    Y[0] = transform(x, [](auto x)
+                     { return cos(x); });
+    Y[1] = transform(x, [](auto x)
+                     { return 0.5 * sin(x); });
+
+    stem(Y, "-o");
+
+    show();
+}
+
+PYBIND11_MODULE(Signal_processor, m)
 {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
-    m.def("sum", &sum, "A function which adds two numbers");
+    m.def("testPlot", &testPlot, "A function which displays example plot");
 }
