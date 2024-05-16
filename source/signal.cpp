95579b98b88e140aa671f6738b509cf5eac16329
3hoot <vietessss@gmail.com>
2024-05-16 12:33:21 +0200

    reworked how signals are generated, handled, etc.

--- source/signal.cpp
+++ source/signal.cpp
@@ -1,16 +1,49 @@
 
+#include <matplot/matplot.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h> // conversion between C++ and Python types
+#include <pybind11/functional.h>
 
-#include "noise.h"
-#include "plot.h"
+#include <vector>
+#include <functional>
+#include <string_view>
+#include <random>
 
+#include "signal.h"
+
+namespace mp = matplot;
 namespace py = pybind11;
 
-PYBIND11_MODULE(signal, handle)
+signal::Basic::Basic(const double start, const double end, const int samples, std::function<double(double)> function)
+    : m_x{mp::linspace(start, end, static_cast<size_t>(samples))},
+      m_y{mp::transform(m_x, function)}
 {
-    handle.doc() = "pybind11 signal processing module"; // optional module docstring
+}
 
-    handle.def("show", &signal::show);
-    handle.def("noise", &signal::getNoise);
+void signal::Basic::show(const std::string_view format_specifiaction) const
+{
+    mp::plot(m_x, m_y, format_specifiaction);
+    mp::show();
+}
+
+void signal::Basic::addNoise(const double amplitude)
+{
+    // Create a random number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> distribution(-amplitude, amplitude);
+
+    // Add random noise to each element of m_y
+    for (double &value : m_y)
+    {
+        value += distribution(gen);
+    }
+}
+
+PYBIND11_MODULE(signal_processing, handle)
+{
+    py::class_<signal::Basic>(handle, "Basic")
+        .def(py::init<double, double, int, std::function<double(double)>>())
+        .def("show", &signal::Basic::show, py::arg("format_specification"))
+        .def("addNoise", &signal::Basic::addNoise, py::arg("amplitude"));
 }
