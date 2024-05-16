38361ec072fa4577fd3267c90cde0660cc3bd51c
3hoot <vietessss@gmail.com>
2024-05-16 15:15:04 +0200

    hotfix

--- source/generic.cpp
+++ source/generic.cpp
@@ -7,19 +7,19 @@
 std::function<double(double)> signal::sin(const Wave &chrs)
 {
     return [=](double x)
-    { return chrs.amplitude * std::sin(chrs.frequency * x + chrs.offset); };
+    { return chrs.amplitude * std::sin(chrs.frequency * x - chrs.offset); };
 }
 
 std::function<double(double)> signal::cos(const Wave &chrs)
 {
     return [=](double x)
-    { return chrs.amplitude * std::cos(chrs.frequency * x + chrs.offset); };
+    { return chrs.amplitude * std::cos(chrs.frequency * x - chrs.offset); };
 }
 
 std::function<double(double)> signal::square(const Wave &chrs)
 {
     return [=](double x)
-    { return chrs.amplitude * std::sin(chrs.frequency * x + chrs.offset) > 0 ? 1 : -1; };
+    { return chrs.amplitude * std::sin(chrs.frequency * x - chrs.offset) > 0 ? 1 : -1; };
 }
 
 std::function<double(double)> signal::sawtooth(const Wave &chrs)
@@ -28,6 +28,6 @@ std::function<double(double)> signal::sawtooth(const Wave &chrs)
     {
         double phase = x * chrs.frequency;
         phase = phase - std::floor(phase);                         // normalize phase to [0, 1)
-        return chrs.amplitude * (2.0 * phase - 1.0) + chrs.offset; // map phase to [-1, 1) and add offset
+        return chrs.amplitude * (2.0 * phase - 1.0) - chrs.offset; // map phase to [-1, 1) and add offset
     };
 }
